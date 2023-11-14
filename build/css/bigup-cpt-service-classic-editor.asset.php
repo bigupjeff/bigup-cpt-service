b5b2e56de3bb61c04af3a5077e95d6eb8996b8cb
wordpressjeff <me@jeffersonreal.com>
2023-11-14 22:55:40 +0000

    first commit

---
+++ build/css/bigup-cpt-service-classic-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '9bb3e9b1d596b4d7e558');
---
+++ build/js/bigup-cpt-service-gutenberg.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '71123a5a9400c81a0c71');
