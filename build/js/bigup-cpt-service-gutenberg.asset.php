215d70f72ff02af1408455fb3452fd2b36a5c5b1
BigupJeff <me@jeffersonreal.uk>
2023-11-16 00:57:12 +0000

    bugfix on image save error

--- build/js/bigup-cpt-service-gutenberg.asset.php
+++ build/js/bigup-cpt-service-gutenberg.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '1e947eccbb7d6c33ba6e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins'), 'version' => '78539228e81c80ae424b');
