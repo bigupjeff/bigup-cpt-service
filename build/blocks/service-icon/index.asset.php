1395defb6f75d5b3435957ab8a33ca9c31e0b69e
wordpressjeff <me@jeffersonreal.com>
2023-12-10 18:26:54 +0000

    update markup

--- build/blocks/service-icon/index.asset.php
+++ build/blocks/service-icon/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '660433be8fc30d6415b6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '98c2e7b94dda6a2d6c68');
