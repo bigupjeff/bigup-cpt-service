604f651891c16d5c79ac28ff533a6c68176dff5b
wordpressjeff <me@jeffersonreal.com>
2023-12-14 14:18:19 +0000

    defaults change

--- build/blocks/service-icon/index.asset.php
+++ build/blocks/service-icon/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '98c2e7b94dda6a2d6c68');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '5ec0d1c952cbc8fa9569');
